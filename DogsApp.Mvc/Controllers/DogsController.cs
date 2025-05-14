d8c7ae39f624ecf58fe91d995e405a22ea725617
NiklasMattsson <niklasmattson1@gmail.com>
2025-05-14 13:36:03 +0200

    controller, models, services, views

---
+++ DogsApp.Mvc/Controllers/DogsController.cs
@@ -0,0 +1,38 @@
+﻿using DogsApp.Mvc.Models;
+using DogsApp.Mvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Mvc.Controllers;
+
+public class DogsController : Controller
+{
+    static DogService dogService = new DogService();
+
+    public DogsController() { }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        var model = dogService.GetAllDogs();
+        return View(model);
+    }
+
+    [HttpGet("create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost("create")]
+    public IActionResult Create(Dog dog)
+    {
+        dogService.AddDog(dog);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+}
+
+
+
+
---
+++ DogsApp.Mvc/Services/DogService.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using DogsApp.Mvc.Models;
+
+namespace DogsApp.Mvc.Services
+{
+    public class DogService
+    {
+        private List<Dog> dogs = [
+            new Dog { Id = 12, Name = "Fiddo", Age = 3},
+            new Dog { Id = 12, Name = "Fiddo", Age = 5},
+            new Dog { Id = 12, Name = "Fiddo", Age = 4},
+            ];
+        public Dog[] GetAllDogs()
+        {
+            return dogs.OrderBy(o => o.Name).ToArray();
+        }
+
+        public Dog GetDogById(int id)
+        {
+            return dogs.SingleOrDefault(o => o.Id == id);
+        }
+        public void AddDog(Dog dog)
+        {
+            dog.Id = dogs.Max(o => o.Id) + 1;
+            dogs.Add(dog);
+        }
+    }
+}
